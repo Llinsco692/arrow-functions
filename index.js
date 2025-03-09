603c565d4b9cf5734263834f34ef6f8d81fa2121
collins muriuki <muriukicollins48@gmail.com>
2025-03-09 14:32:43 +0300

    created  arrow functions

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide =  function division(num1, num2){
+   return num1 / num2;
+}
+divide(2000,100);
+ 
+const square = (x => x * x);
+square(2);
+
+const add = (num1, num2) => {
+    return num1 + num2;
+}
+add(20,30);
